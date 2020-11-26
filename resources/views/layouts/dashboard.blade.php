6b275e11c40105743c24906c74779d7972472780
Reza Rachmanuddin <rezarahmanudin@gmail.com>
2020-11-26 08:56:46 +0700

    style(main.dashboard): add greating text and remove breadcrumb

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -262,12 +262,6 @@
             <div class="col-sm-6">
               <h1 class="m-0 text-dark">@yield('heading')</h1>
             </div><!-- /.col -->
-            <div class="col-sm-6">
-              <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active">Dashboard v1</li>
-              </ol>
-            </div><!-- /.col -->
           </div><!-- /.row -->
         </div><!-- /.container-fluid -->
       </div>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,5 +3,9 @@
 @section('heading', 'Dashboard')
 
 @section('content')
-    
+<div class="card">
+  <div class="card-body">
+    <h2>Selamat Datang, {{ auth()->user()->name }}</h2>
+  </div>
+</div>
 @endsection
